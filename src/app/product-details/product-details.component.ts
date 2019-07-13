43dd4585e76861c85a1324cfcc35f81dbe108433
James McMeeken <james@jamesmcmeeken.com>
2019-07-13 13:35:27 +0100

    Add cart service and cart component
    - Added Cart Service
    - Added Cart component
    - Added addToCart method to Product Details component

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { products } from '../products';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-product-details',
@@ -11,12 +12,20 @@ import { products } from '../products';
 export class ProductDetailsComponent implements OnInit {
   product;
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private route: ActivatedRoute,
+    // Injecting the cart service into this component here:
+    private cartService: CartService
+  ) {}
+
+  addToCart(product) {
+    this.cartService.addToCart(product);
+    window.alert('This product has been added to the cart!');
+  }
 
   ngOnInit() {
     this.route.paramMap.subscribe(params => {
       this.product = products[+params.get('productId')]
-    })
+    });
   }
-
 }
